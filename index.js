3c08447200a83bf3a3f1ac2e566ca346f0eb8a5f
Kyle Samuel <ksamuel92@gmail.com>
2021-02-05 20:18:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-var kittens = [] //define your array here
-
+var kittens = ['Milo', 'Otis', 'Garfield'];
 // Add your functions and code here
+
+ function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens;
+}
+
+ function destructivelyPrependKitten  (name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten (name) {
+  return newArray = [...kittens, name];}
+
+function prependKitten (name) {
+  return newArray = [name, ...kittens];
+}
+
+function removeLastKitten () {
+ return newArray = kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten () {
+  return newArray = kittens.slice(0,1);
+}
+
